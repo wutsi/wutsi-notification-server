a3e31f0cf419f565d3c870eedfebbecf56d1d3b7
wutsi <herve.tchepannou@gmail.com>
2021-11-21 01:28:26 -0500

    add tenant configuration

---
+++ src/main/kotlin/com/wutsi/platform/notification/config/TenantApiConfiguration.kt
@@ -0,0 +1,42 @@
+package com.wutsi.platform.notification.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.wutsi.platform.core.security.TokenProvider
+import com.wutsi.platform.core.security.feign.FeignApiKeyRequestInterceptor
+import com.wutsi.platform.core.security.feign.FeignAuthorizationRequestInterceptor
+import com.wutsi.platform.core.tracing.feign.FeignTracingRequestInterceptor
+import com.wutsi.platform.tenant.Environment.PRODUCTION
+import com.wutsi.platform.tenant.Environment.SANDBOX
+import com.wutsi.platform.tenant.WutsiTenantApi
+import com.wutsi.platform.tenant.WutsiTenantApiBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.core.env.Profiles
+
+@Configuration
+public class TenantApiConfiguration(
+    private val tokenProvider: TokenProvider,
+    private val tracingRequestInterceptor: FeignTracingRequestInterceptor,
+    private val apiKeyRequestInterceptor: FeignApiKeyRequestInterceptor,
+    private val mapper: ObjectMapper,
+    private val env: Environment
+) {
+    @Bean
+    fun tenantApi(): WutsiTenantApi =
+        WutsiTenantApiBuilder().build(
+            env = environment(),
+            mapper = mapper,
+            interceptors = listOf(
+                apiKeyRequestInterceptor,
+                tracingRequestInterceptor,
+                FeignAuthorizationRequestInterceptor(tokenProvider)
+            )
+        )
+
+    private fun environment(): com.wutsi.platform.tenant.Environment =
+        if (env.acceptsProfiles(Profiles.of("prod")))
+            PRODUCTION
+        else
+            SANDBOX
+}
